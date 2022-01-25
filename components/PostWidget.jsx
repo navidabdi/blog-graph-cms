86bdd8a22405d4560a23da533e34f269307f6183
navidabdi <artsanaat@gmail.com>
2022-01-25 17:54:05 +0330

    First Commit

---
+++ components/PostWidget.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const PostWidget = () => {
+  return (
+    <div>
+      <h2>Post Widget</h2>
+    </div>
+  )
+}
+
+export default PostWidget
---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+import { PostCard, Categories, PostWidget } from '../components'
+const posts = [
+  { title: 'React testing', excerpt: 'Learn React testing' },
+  {
+    title: 'React With Tailwind CSS',
+    excerpt: 'Learn React With Tailwind CSS',
+  },
+]
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Blog CMS</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="container mx-auto mb-8 px-10">
+        <div className="grid grid-cols-1 gap-12 lg:grid-cols-12">
+          <div className="col-span-1 lg:col-span-8">
+            {posts.map((post, index) => (
+              <PostCard post={post} key={index} />
+            ))}
+          </div>
+        </div>
+        <div className="col-span-1 lg:col-span-4">
+          <div className="relative top-8 lg:sticky">
+            <PostWidget />
+            <Categories />
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
