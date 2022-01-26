7eabd457f8d56947647ec032c29d0828f1f460a7
navidabdi <artsanaat@gmail.com>
2022-01-26 18:59:26 +0330

    Complete The Categories Component

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import Link from 'next/link'
+import { getCategories } from '../services'
 const Categories = () => {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories))
+  }, [])
+
   return (
-    <div>
-      <h2>Categories</h2>
+    <div className="mb-8 rounded-lg bg-white p-8 shadow-lg">
+      <h3 className="mb-8 border-b pb-4 text-xl font-semibold">Categories</h3>
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`}>
+          <span className="mb-3 block cursor-pointer pb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
     </div>
   )
 }
