6764e85fee7a6435fb5aa0115de7cdcc16f5f76b
navidabdi <artsanaat@gmail.com>
2022-01-26 17:33:40 +0330

    Add PostCard Data

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState, useEffect } from 'react'
 import Link from 'next/link'
 
 const categories = [
@@ -7,27 +7,35 @@ const categories = [
 ]
 
 const Header = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  useEffect(() => {
+    setIsLoading(true)
+  }, [])
   return (
-    <div className="container mx-auto mb-8 px-10">
-      <div className="inline-block w-full border-b border-blue-400 py-8">
-        <div className="block md:float-left">
-          <Link href="/">
-            <span className="cursor-pointer text-4xl font-bold text-white">
-              Blog CMS
-            </span>
-          </Link>
+    <>
+      {isLoading && (
+        <div className="container mx-auto mb-8 px-10">
+          <div className="inline-block w-full border-b border-blue-400 py-8">
+            <div className="block md:float-left">
+              <Link href="/">
+                <span className="cursor-pointer text-4xl font-bold text-white">
+                  Blog CMS
+                </span>
+              </Link>
+            </div>
+            <div className="hidden md:float-left md:contents">
+              {categories.map((category, index) => (
+                <Link href={`/category/${category.slug}`} key={index}>
+                  <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
+                    {category.name}
+                  </span>
+                </Link>
+              ))}
+            </div>
+          </div>
         </div>
-        <div className="hidden md:float-left md:contents">
-          {categories.map((category, index) => (
-            <Link href={`/category/${category.slug}`} key={index}>
-              <span className="mt-2 ml-4 cursor-pointer align-middle font-semibold text-white md:float-right">
-                {category.name}
-              </span>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </div>
+      )}
+    </>
   )
 }
 
