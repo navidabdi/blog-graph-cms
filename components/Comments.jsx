1a167aa5b5aba7cab9c5d8b5010a604c27db8802
navidabdi <artsanaat@gmail.com>
2022-01-29 17:49:41 +0330

    Show Submitted Comments Into Article Page

--- components/Comments.jsx
+++ components/Comments.jsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import moment from 'moment'
+import parse from 'html-react-parser'
+import { getComments } from '../services'
+import { comment } from 'postcss'
 
-const Comments = () => {
-  return <div>Comments</div>
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([])
+
+  useEffect(() => {
+    getComments(slug).then((results) => setComments(results))
+  }, [])
+
+  return (
+    <div>
+      {comments.length > 0 && (
+        <div className="mb-8 rounded-lg bg-white p-8 pb-12 shadow-lg">
+          <h3 className="mb-8 border-b pb-4 text-xl font-semibold">
+            {comments.length} Comments
+          </h3>
+          {comments.map((comment, index) => (
+            <div key={index} className="mb-4 border-b border-gray-100 pb-4">
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span> on{' '}
+                {moment(comment.createdAt).format('MMM DD, YYYY')}
+              </p>
+              <p className="whitespase-pre-line w-full text-gray-600 ">
+                {parse(comment.comment)}
+              </p>
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default Comments
--- services/index.js
+++ services/index.js
@@ -131,3 +131,16 @@ export const submitComment = async (obj) => {
 
   return result.json()
 }
+export const getComments = async (slug) => {
+  const query = gql`
+    query GetComments($slug: String!) {
+      comments(where: { post: { slug: $slug } }) {
+        name
+        createdAt
+        comment
+      }
+    }
+  `
+  const results = await request(graphqlAPI, query, { slug })
+  return results.comments
+}
